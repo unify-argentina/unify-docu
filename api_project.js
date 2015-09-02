0c1e4d4c27cf591521b9ded8462a6404184fd835
Joel <90joelmarquez@gmail.com>
2015-09-02 00:23:59 -0300

    Docu actualizada

--- api_project.js
+++ api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-09-01T23:52:20.251Z",
+    "time": "2015-09-02T03:23:22.558Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
