703d8bea30a76f7a6f825a285aac2e531f9de72a
Joel Márquez <joelm@hellobaytree.com>
2015-09-17 14:21:09 -0300

    Docu actualizada

--- api_project.js
+++ api_project.js
@@ -1,19 +1,1 @@
-define({
-  "name": "Unify API",
-  "version": "0.0.1",
-  "description": "This is the documentation of the Unify API",
-  "title": "Unify API",
-  "url": "http://localhost:8080",
-  "sampleUrl": "http://localhost:8080",
-  "template": {
-    "withCompare": true,
-    "withGenerator": true
-  },
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2015-09-16T04:57:23.068Z",
-    "url": "http://apidocjs.com",
-    "version": "0.13.1"
-  }
-});
+define({  "name": "Unify API",  "version": "0.0.1",  "description": "This is the documentation of the Unify API",  "title": "Unify API",  "url": "http://localhost:8080",  "sampleUrl": "http://localhost:8080",  "template": {    "withCompare": true,    "withGenerator": true  },  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-09-17T17:21:01.330Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
