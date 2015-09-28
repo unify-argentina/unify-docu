6a122d50ea73fb9a8a247adf2170b8d280cb8ee1
Joel <90joelmarquez@gmail.com>
2015-09-28 02:02:00 -0300

    Docu actualizada

--- api_project.js
+++ api_project.js
@@ -3,8 +3,8 @@ define({
   "version": "0.0.1",
   "description": "This is the documentation of the Unify API",
   "title": "Unify API",
-  "url": "http://localhost:8080",
-  "sampleUrl": "http://localhost:8080",
+  "url": "http://api.myunify.io",
+  "sampleUrl": "http://api.myunify.io",
   "template": {
     "withCompare": true,
     "withGenerator": true
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-09-28T04:56:58.465Z",
+    "time": "2015-09-28T05:01:48.095Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
